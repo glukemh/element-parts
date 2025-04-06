ced0c0ad1ea16f207565e0649cf0bd3393b7f3fb
GLukeM-H <glukemh@gmail.com>
2025-04-06 18:02:55 -0500

    custom element builder

--- components/my-root.js
+++ components/my-root.js
@@ -1,3 +1,22 @@
+import { ShData } from '../lib/sh.js';
+import { attributeRecord } from '../lib/element-config.js';
+import { define } from '../lib/custom-element.js';
+import config from './my-root.json' with { type: 'json' };
+
+class MyRoot extends define().attributes(attributeRecord(config)).tag(config.name).class() {
+  static {
+    this.lifecycleCallbacks({
+      attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'title') {
+          if (newValue === 'Web Page') {
+          }
+        }
+      }
+    });
+  }
+}
+
+
 export default {
   'title-part': (el) => {
     el.textContent = '~Dynamic~';
