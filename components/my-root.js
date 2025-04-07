9c1d5c2b2e839955fe4005cdcc6d235a0cbb4819
GLukeM-H <glukemh@gmail.com>
2025-04-06 19:34:52 -0500

    data bindings

--- components/my-root.js
+++ components/my-root.js
@@ -1,24 +1,40 @@
-import { ShData } from '../lib/sh.js';
+/** @import {ElementCallbackConfig} from "../lib/sh.js" */
+import { define } from '../lib/sh.js';
 import { attributeRecord } from '../lib/element-config.js';
-import { define } from '../lib/custom-element.js';
 import config from './my-root.json' with { type: 'json' };
 
-class MyRoot extends define().attributes(attributeRecord(config)).tag(config.name).class() {
+/** @satisfies {ElementCallbackConfig} */
+const dataConfig = /** @type {const} */({
+  titleEl: {
+    expect: ['h1', 'h2', 'span'],
+  },
+  randomNumber: {
+    expect: ['div', 'span'],
+  }
+});
+
+class MyRoot extends define().shDataConfig(dataConfig).attributes(attributeRecord(config)).tag(config.name).class() {
   static {
+    console.debug(`Registering ${this.tag}`);
     this.lifecycleCallbacks({
+      connectedCallback() {
+        console.debug(`Connected ${MyRoot.tag}`);
+        this.hydrate();
+      },
       attributeChangedCallback(name, oldValue, newValue) {
         if (name === 'title') {
           if (newValue === 'Web Page') {
           }
         }
       }
+    }).shDataCallbacks({
+      titleEl(el) {
+        el.textContent = this.attrValue('title') ?? 'Default Title';
+      },
+      randomNumber(el) {
+        el.textContent = (Math.random() * 100).toFixed(2);
+      }
     });
+    this.register();
   }
 }
-
-
-export default {
-  'title-part': (el) => {
-    el.textContent = '~Dynamic~';
-  }
-};
